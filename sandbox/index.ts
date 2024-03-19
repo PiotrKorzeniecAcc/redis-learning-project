c877cc1c4985d6473c4bcd094dc1ed70cd8b637e
Korzeniec <piotr.korzeniec@accenture.com>
2024-03-19 15:20:59 +0100

    example functionality of html content with implementation of get and set redis functions

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,14 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    await client.hSet('carHash', {
+        color: 'red',
+        year: 1950
+    });
+
+    const car = await client.hGetAll('carHash');
+
+    console.log(car);
+};
 run();
