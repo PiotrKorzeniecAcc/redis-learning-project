6629bc006e6231f0648e4b625bc2178dc548b2d5
Korzeniec <piotr.korzeniec@accenture.com>
2024-03-20 13:26:30 +0100

    functionality for create and get items

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,43 @@
 import type { CreateItemAttrs } from '$services/types';
+import { client } from '$services/redis';
+import { serialize } from './serialize';
+import { deserialize } from './deserialize';
+import { genId } from '$services/utils';
+import { itemsKey } from '$services/keys';
 
-export const getItem = async (id: string) => {};
+export const getItem = async (id: string) => {
+    const item = await client.hGetAll(itemsKey(id));
 
-export const getItems = async (ids: string[]) => {};
+    if (Object.keys(item).length === 0) {
+        return null;
+    }
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+    return deserialize(id, item);
+};
+
+export const getItems = async (ids: string[]) => {
+    const commands = ids.map((id) => {
+        // foreach ids as id add to command variable command client.hGetAll(id)
+        return client.hGetAll(itemsKey(id));
+    });
+
+    const results = await Promise.all(commands);
+
+    results.map((result, i) => {
+        // foreach result determine if object exists and return it deserialized (with given id)
+        if (Object.keys(result).length === 0) {
+            return null;
+        }
+
+        return deserialize(ids[i], result);
+    })
+};
+
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+    const id = genId();
+    const serialized = serialize(attrs);
+
+    await client.hSet(itemsKey(id), serialized);
+
+    return id;
+};
--- src/services/queries/items/serialize.ts
+++ src/services/queries/items/serialize.ts
@@ -1,3 +1,9 @@
 import type { CreateItemAttrs } from '$services/types';
 
-export const serialize = (attrs: CreateItemAttrs) => {};
+export const serialize = (attrs: CreateItemAttrs) => {
+    return {
+        ...attrs,
+        createdAt: attrs.createdAt.toMillis(), //serialize datetime vale to epoch format (miliseconds)
+        endingAt: attrs.endingAt.toMillis()
+    };
+};
