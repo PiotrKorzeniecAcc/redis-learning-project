2b2beaef027108e840ec338062342b68f2cbaab8
Korzeniec <piotr.korzeniec@accenture.com>
2024-03-25 17:45:12 +0100

    example of lua script in place of typescript redis client call

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,23 +1,5 @@
 import { client } from "$services/redis";
-import { itemsKey, itemsByViewsKey, itemsViewsKey } from "$services/keys";
 
 export const incrementView = async (itemId: string, userId: string) => {
-    const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
-
-    if (inserted) {
-        return Promise.all(
-            [
-                client.hIncrBy(         // Increment the item hash views
-                    itemsKey(itemId),
-                    'views',
-                    1
-                    ),
-                client.zIncrBy(         // Increment the sorted set score
-                    itemsByViewsKey(),
-                    1,
-                    itemId
-                    )
-            ]
-            );
-    }
+    return client.incrementView(itemId, userId);
 };
