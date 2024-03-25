c62d9ef0c746dce8fdf230d64e504146f3466b2e
Korzeniec <piotr.korzeniec@accenture.com>
2024-03-25 11:27:57 +0100

    hyperloglog pfAdd function added for unique user-item-views functionality

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,19 +1,23 @@
 import { client } from "$services/redis";
-import { itemsKey, itemsByViewsKey } from "$services/keys";
+import { itemsKey, itemsByViewsKey, itemsViewsKey } from "$services/keys";
 
 export const incrementView = async (itemId: string, userId: string) => {
-    return Promise.all(
-        [
-            client.hIncrBy(         // Increment the item hash views
-                itemsKey(itemId),
-                'views',
-                1
-            ),
-            client.zIncrBy(         // Increment the sorted set score
-                itemsByViewsKey(),
-                1,
-                itemId
-            )
-        ]
-    );
+    const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+
+    if (inserted) {
+        return Promise.all(
+            [
+                client.hIncrBy(         // Increment the item hash views
+                    itemsKey(itemId),
+                    'views',
+                    1
+                    ),
+                client.zIncrBy(         // Increment the sorted set score
+                    itemsByViewsKey(),
+                    1,
+                    itemId
+                    )
+            ]
+            );
+    }
 };
